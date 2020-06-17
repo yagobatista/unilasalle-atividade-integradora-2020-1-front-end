308cb2056d5d34ed0c4828c9cac3a31eb16a3679
Yago Batista da Silva <yagobatistasilva@gmail.com>
2020-06-16 23:05:16 -0300

    Updates

---
+++ precache-manifest.f66b94bc6bed7c380dc9c9b653ea7949.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f59592761f478d01f34991bb714c9ba",
+    "url": "/unilasalle-atividade-integradora-2020-1-front-end/index.html"
+  },
+  {
+    "revision": "a307707e0d9954745878",
+    "url": "/unilasalle-atividade-integradora-2020-1-front-end/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4132bedc53144b3ec3ee",
+    "url": "/unilasalle-atividade-integradora-2020-1-front-end/static/js/2.f30b4f01.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/unilasalle-atividade-integradora-2020-1-front-end/static/js/2.f30b4f01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a307707e0d9954745878",
+    "url": "/unilasalle-atividade-integradora-2020-1-front-end/static/js/main.1c4eac6d.chunk.js"
+  },
+  {
+    "revision": "ef7c306be0fa05e6734d",
+    "url": "/unilasalle-atividade-integradora-2020-1-front-end/static/js/runtime-main.060aef6e.js"
+  }
+]);
